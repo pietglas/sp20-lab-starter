f3f574b78221e88e58301b19cdbfd5a6ae86eaf4
piet@piet-Latitude-E7440 <pietglas77@gmail.com>
2020-07-15 00:29:25 +0200

    wrote ll_cycle.c, finished tests successfully

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,18 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
-    return 0;
+	if (head == NULL)
+		return 0;
+    node *hare = head;
+    node *tortoise = head;
+    for (int i = 0; ; i++) {
+    	if (hare->next == NULL || hare->next->next == NULL) 
+    		return 0;
+    	else {
+    		hare = hare->next->next;
+    		tortoise = tortoise->next;
+    		if (hare == tortoise)
+    			return -1; 
+    	}
+    }
 }
--- lab01/no_segfault_ex.c
+++ lab01/no_segfault_ex.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main() {
     int a[5] = {1, 2, 3, 4, 5};
-    unsigned total = 0;
+    unsigned int total = 0;
+    printf("%lu\n", sizeof(a));
     for (int j = 0; j < sizeof(a); j++) {
-        total += a[j];
+        total += a[j];	// accesses uninitialized memory, undefined behavior
     }
     printf("sum of array is %d\n", total);
 }
