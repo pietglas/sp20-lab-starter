19ac2da87e5be9375b25f2b3a562ecdcf87f6d47
piet@piet-Latitude-E7440 <pietglas77@gmail.com>
2020-07-15 19:23:32 +0200

    completed bit operations exercise

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -5,23 +5,25 @@
 // Assume 0 <= n <= 31
 unsigned get_bit(unsigned x,
                  unsigned n) {
-    // YOUR CODE HERE
-    // Returning -1 is a placeholder (it makes
-    // no sense, because get_bit only returns 
-    // 0 or 1)
-    return -1;
+    return (x << (31 - n)) >> 31;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
-    // YOUR CODE HERE
+    if (v) {    // if v == 1, we can use | 
+        *x = (*x) | (v << n);
+    }
+    else {  // if v == 0, negate v and x, use | and negate x again. 
+        *x = ~(~(*x) | (1 << n));
+    }
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
-    // YOUR CODE HERE
+    unsigned bit = ((~get_bit(*x, n)) << 31) >> 31;
+    set_bit(x, n, bit);
 }
 
